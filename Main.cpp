d8a26b2d4d60c4c86af834b90a2538d039435008
Debdipbanerjee <debdipbanerjee7@gmail.com>
2022-02-14 01:34:22 -0700

    Scaling vectors

--- Main.cpp
+++ Main.cpp
@@ -29,7 +29,8 @@ public:
 		return length;
 	}
 
-	
+	Vector operator*(float s) const;
+	Vector operator/(float s) const;
 	
 };
 
@@ -79,6 +80,25 @@ Vector operator-(Point a, Point b)
 
 }
 
+Vector Vector::operator*(float scale) const
+{
+	Vector v;
+	v.x = x * scale;
+	v.y = y * scale;
+
+	return v;
+}
+
+Vector Vector::operator/(float scale) const
+{
+	Vector v;
+	v.x = x / scale;
+	v.y = y / scale;
+
+	return v;
+}
+
+
 
 
 
@@ -96,7 +116,7 @@ Point WhoIsCloser(Point p1, Point p2, Point p3)
 	Vector v13 = GiveMeVector(p1, p3);
 
 	if (v12.Length() < v13.Length()) return p2;
-	if (v12.Length() > v13.Length()) return p3;
+	else return p3;
 }
 
 int main()
@@ -105,8 +125,13 @@ int main()
 	Point p2(5, 7, "p2");
 	Point p3(1, 2, "p3");
 
-	cout << "the closest is " << WhoIsCloser(p1, p2, p3).name << endl;
+	//cout << "the closest is " << WhoIsCloser(p1, p2, p3).name << endl;
+	
+	Vector v = (GiveMeVector(p1,p2) * 2);
+	Vector v1 = (GiveMeVector(p1, p2) / 2);
 
+	cout << "scaled vector is " << v.x << "," << v.y << endl;
+	cout << "scaled vector is " << ceil(v1.x) << "," << v1.y << endl;
 
 
 	return 0;
