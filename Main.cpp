55df1ce6bfe7d338d8b984564020ae182dbb0ecd
Debdipbanerjee <debdipbanerjee7@gmail.com>
2022-03-08 14:08:26 -0700

    Insert Node & Print function implemented

--- Main.cpp
+++ Main.cpp
@@ -7,34 +7,95 @@ class Node
 public:
     int value;
     Node* next;   
+
+    Node()
+    {
+        this->value = 0;
+        this->next = nullptr;
+    }
+
 };
 
- 
-void Printlist(Node* n)
+class  LinkedList
 {
-    while (n != NULL)
+public:
+    Node* head = nullptr;
+
+    void InsertNode();
+    void PrintList(Node* n);
+    void AddNode();
+   
+
+
+};
+
+
+void LinkedList:: InsertNode()
+{
+    Node* newNode = new Node();
+    cout << "Enter value: ";
+    cin >> newNode->value;
+
+    if (head == nullptr)
     {
-        cout << n->value << "->";
+        head = newNode;
+    }
+    else
+    {
+        Node* temp = head;
+
+        while (temp->next != nullptr)
+        {
+            temp = temp->next;
+        }
+
+        temp->next = newNode;
+    }
+
+   
+
+}
+
+void LinkedList::PrintList(Node* n)
+{
+    while (n!= nullptr)
+    {
+        cout << n->value << endl;
         n = n->next;
     }
 }
 
+
+
 int main()
 {
+    char c = getchar();
+    LinkedList L;
 
-    Node* head = new Node();
-    Node* second = new Node();
-    Node* third = new Node();
-    
+    do
+    {
+        int option;
+        cout << "Prss 1 for insert a node" << endl;
+        cout << "Prss 2 for print the list" << endl;
+        cin >> option;
 
-    head->value = 1;
-    second->value = 2;
-    third->value = 3;
 
-    head->next = second;
-    second->next = third;
-    third->next = nullptr;
 
-    Printlist(head);
+        switch (option)
+        {
+        case 1:
+            L.InsertNode();
+            break;
+        case 2:
+            L.PrintList(L.head);
+            break;
+        default:
+            break;
+        }
+
+    } while (c != 'E');
+   
+  
+   
 }
 
